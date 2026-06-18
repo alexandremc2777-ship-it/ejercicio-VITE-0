58dfdc3a50adaa12ef71ca72cb785d376d8ebb1a
alexandremc2777-ship-it <alexandremc2777@gmail.com>
2026-06-18 09:16:19 +0200

    Cambios html y JS

---
+++ src/assets/Js/assets/_artacordeon01.js
@@ -0,0 +1,16 @@
+
+
+const botones= document.querySelectorAll(".mostrar")
+
+// recorro con forEach y funciones flecha. ClassicList reogeria un estilo de la lista y con toggle haría la función acordeón, de poner y quitar el estilo.
+botones.forEach((item)=>{
+
+    item.addEventListener("click", ()=>{
+        // parrafoDesplegable.classList.toggle("open")
+        // const superior = item.parentElement
+        // const miParrafo = superior.querySelector(".desplegable")
+
+        const miParrafo = item.previousElementSibling
+        miParrafo.classList.toggle("open")
+    })
+})
--- src/assets/Js/main.js
+++ src/assets/Js/main.js
@@ -1,2 +1,3 @@
 import '../scss/style.scss'
 import './assets/_artJs.js'
+import './assets/_artacordeon01.js'
