33b6432fa69a4b788e3257cec4d94d71e2f8d5b2
alexandremc2777-ship-it <alexandremc2777@gmail.com>
2026-06-19 14:00:03 +0200

    cambios html y carrusel

--- src/assets/Js/assets/_artSlider.js
+++ src/assets/Js/assets/_artSlider.js
@@ -4,9 +4,12 @@ export default function artSlider() {
     $carrousel.forEach(($el) => {
 
         const $pista = $el.querySelector(".artSlider01__pista");
+        const $dots = $el.querySelectorAll(".artSlider01__track__dot");
         const $sliders = document.querySelectorAll(".artSlider01__slide");
 
-        // Compro
+        console.log($dots);
+
+        // Comprobar que nuestra pista y los sliders existas dentro del DOM
         if (!$pista || $sliders.length === 0) return;
 
         let contador = 0;
@@ -15,15 +18,41 @@ export default function artSlider() {
         const tiempoEspera = 3000;
 
         $sliders.forEach(($slide) => {
-            console.log($slide);
+           const $copia = $slide.cloneNode(true);
+           $pista.appendChild($copia);
         })
 
+        // El setinterval se ejecutará cada 3000 milisegundos
         setInterval(() => {
-            console.log(`Contador: ${contador}`);
-            console.log(`Tiempo: ${(tiempoEspera * contador) / 1000}s`);
+        //     console.log(`Contador: ${contador}`);
+        //     console.log(`Tiempo: ${(tiempoEspera * contador) / 1000}s`);
             contador++
 
+            $pista.style.transform= `translateX(-${100 * contador}%)`
+            $pista.style.transition = `transform ${tiempoTransicion}ms`
+
+            $dots[contador - 1]?.classList.remove("active");
+            if ($dots[contador]){
+                $dots[contador].classList.add("active");
+            } else {
+                $dots[0].classList.add("active");
+            }
+        // Hay que validad  que el contador sea igual al número de la última imagen
+        if(contador===$sliders.length){
+            setTimeout(()=>{
+            contador = 0;
+            $pista.style.transform= `translateX(0)`;
+            $pista.style.transition = "none";
+            }, tiempoTransicion);
+           
+        }
+        // Reiniciar la posición del visor para mostrar el primer slider
+        
         }, tiempoEspera);
+
+        // setTimeout(()=> {
+        //     console.log(`Me ejecuto cada ${tiempoEspera}s`);
+        // }, tiempoEspera);
 
     });
 
